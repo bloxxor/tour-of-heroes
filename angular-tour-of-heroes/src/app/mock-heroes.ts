ea9674f96bfef7a14b8ba64a2c5e3ac03647f811
Florian Gabele <gabele@ponton.de>
2023-09-01 14:03:00 +0200

    Added hero detail component

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import {Hero} from './hero';
+
+/**
+ * Mock list of heroes
+ */
+export const HEROES: Hero[] = [
+  {id: 12, name: 'Dr. Doctor'},
+  {id: 13, name: 'Bombasto'},
+  {id: 14, name: 'Endoflator'},
+  {id: 15, name: 'Dick Wiggler'},
+  {id: 16, name: 'One-Punch-Man'},
+  {id: 17, name: 'Space Hurricane'},
+  {id: 18, name: 'Magma'},
+  {id: 19, name: 'Tornado'},
+  {id: 20, name: 'Ghostrain'},
+]
